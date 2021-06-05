6c05d117d410ccb73ba9f7c0bd0239f25b5d1923
vlresht <vlreshtanenko@gmail.com>
2021-06-05 12:02:13 +0300

    add filters to gallery

--- src/index.js
+++ src/index.js
@@ -11,3 +11,26 @@ $(document).ready(function() {
         slidesToShow: 4
     })
 })
+
+let filters = $('.slider__filter');
+filters[0].style.width = "380px";
+filters[0].style.height = "161px";
+filters[0].style.bottom = "271px";
+
+filters[1].style.width = "160px";
+filters[1].style.height = "380px";
+filters[1].style.bottom = "380px";
+filters[1].style.left = "140px";
+
+filters[2].style.width = "160px";
+filters[2].style.height = "380px";
+filters[2].style.bottom = "380px";
+filters[2].style.left = "140px";
+
+filters[3].style.width = "380px";
+filters[3].style.height = "161px";
+filters[3].style.bottom = "339px";
+
+filters[4].style.width = "380px";
+filters[4].style.height = "161px";
+filters[4].style.bottom = "191px";
